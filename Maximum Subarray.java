dc76e9d164b5aaca77b47c59b45d9e832c44b3d3
SitanshuChauhan <83861158+SitanshuChauhan@users.noreply.github.com>
2022-04-05 14:37:17 +0530

    Add files via upload

---
+++ Maximum Subarray.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/maximum-subarray/
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+      int max=Integer.MIN_VALUE;
+      int sum=0;
+        for(int i=0;i<nums.length;i++){
+            if(sum<0)
+                sum=nums[i];
+            else
+                sum+=nums[i];
+            if(max<sum)
+                max=sum;
+        }
+        return max;
+    }
+}
